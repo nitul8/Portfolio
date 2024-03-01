90837a5ef15a49338862d64b92745edd82027f1f
Nitul Das <137324926+nitul8@users.noreply.github.com>
2024-03-02 00:36:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function hi() {
+    document.querySelector(".hi").innerHTML = "About";
+}
+
+function hi1() {
+    document.querySelector(".hi").innerHTML = "Hello,";
+}
+
+function im() {
+    document.querySelector(".im").innerHTML = "Experience";
+}
+
+function im1() {
+    document.querySelector(".im").innerHTML = "My Name is";
+}
+
+function nd() {
+    document.querySelector(".nd").innerHTML = "Contact";
+}
+
+function nd1() {
+    document.querySelector(".nd").innerHTML = "Nitul Das";
+}
+
+function lg() {
+    document.querySelector(".logo").innerHTML = "nitul.das";
+}
+
+function lg1() {
+    document.querySelector(".logo").innerHTML = "nitul.";
+}
