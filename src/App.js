84c6b39f724ed2bd60c60d44f49b1d3d4acd90ab
Nitul Das <ndas6732@gmail.com>
2024-04-02 23:26:43 +0530

    modified:   postcss.config.js 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	modified:   src/index.js 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
-
 function App() {
-    return <></>;
+    return (
+        <>
+            <div className="text-center font-bold text-2xl">
+                <h1>This is Neel here!</h1>
+            </div>
+            <p className="summary">
+                You can find my thoughts here.
+                <br />
+                <br />
+                And <b>pictures</b> of scientists!
+            </p>
+        </>
+    );
 }
 
 export default App;
