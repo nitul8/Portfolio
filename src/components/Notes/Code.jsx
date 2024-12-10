51e2deaf93810a6ea930f230b0e66b4673fdeb34
Nitul Das <ndas6732@gmail.com>
2024-12-10 21:17:50 +0530

    Day-2 Updated

---
+++ src/components/Notes/Code.jsx
@@ -0,0 +1,9 @@
+const Code = ({children}) => {
+    return (
+        <code className="text-red-500 text-sm bg-gray-200 rounded-sm">
+            {children}
+        </code>
+    );
+};
+
+export default Code;
--- src/components/Notes/ReactJS/Week1.jsx
+++ src/components/Notes/ReactJS/Week1.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import Code from "../Code";
 import {Prism as SyntaxHighlighter} from "react-syntax-highlighter";
 import {oneDark} from "react-syntax-highlighter/dist/esm/styles/prism";
+
+import pic from "/Users/nitul/Documents/My-Projects/CodersCave/portfolio/src/assets/photo/components.png";
 
 const Week1 = () => {
     return (
@@ -44,9 +47,7 @@ const Week1 = () => {
                         <br />
                         <strong>File Naming Convention:</strong> JSX files are
                         typically named using the format{" "}
-                        <code className="text-red-600 text-sm">
-                            "file-name.jsx"
-                        </code>
+                        <Code>"file-name.jsx"</Code>
                     </p>
                     <br />
                     <p className="text-lg">
@@ -120,15 +121,132 @@ const App = () => {
                             </li>
                             <li className="font-semibold">
                                 Attribute `class` is{" "}
-                                <code className="text-red-500 text-sm">
-                                    'className'
-                                </code>
-                                .
+                                <Code className="font-light">'className'</Code>.
                             </li>
                             <li className="font-semibold">
                                 Elements must be closed.
                             </li>
                         </ol>
+                    </p>
+                </section>
+
+                {/* Components */}
+                <section className="mb-8 w-full">
+                    <h2 className="text-2xl font-semibold mb-4 font-heading text-black dark:text-white">
+                        Components
+                    </h2>
+                    <p className="text-lg">
+                        <strong>Components</strong> are independent and reusable
+                        bits of code. They are{" "}
+                        <strong>JavaScript or JSX files</strong>. They serve the
+                        same purpose as <strong>JS functions</strong>. But works
+                        in isolation and returns{" "}
+                        <strong>HTML via a render function</strong>.
+                    </p>
+                    <div className="flex justify-center">
+                        <img src={pic} alt="Components Pic" className="w-96" />
+                    </div>
+                    <p className="text-lg">
+                        Just like this <strong>React App</strong> is divided
+                        into multiple components which we can use multiple times
+                        as we need it. And it helps us in editing and changing
+                        in a particular part of App rather than changing the
+                        whole App.
+                    </p>
+                    <br />
+                    <p className="text-lg">
+                        <strong>Types of Components in React:</strong>
+                        <ol className="list-decimal list-inside text-lg pl-5 space-y-2">
+                            <li>
+                                <span className="font-semibold">
+                                    Functional Components
+                                </span>
+                                <ul className="list-disc list-inside">
+                                    <li>
+                                        Written as plain JavaScript functions.
+                                    </li>
+                                    <li>
+                                        Use React hooks (e.g.,{" "}
+                                        <Code>useState</Code>,
+                                        <Code>useEffect</Code>) to manage state
+                                        and lifecycle methods.
+                                    </li>
+                                    <li>Lightweight and easier to read.</li>
+                                </ul>
+                                <SyntaxHighlighter
+                                    language="jsx"
+                                    style={oneDark}
+                                    className="rounded mt-4 text-sm"
+                                >
+                                    {`import React from "react";
+
+export const Greeting = (props) => {
+  return <h1>Hello, {props.name}!</h1>;
+};`}
+                                </SyntaxHighlighter>
+                            </li>
+                            <li>
+                                <span className="font-semibold">
+                                    Class Components
+                                </span>
+                                <ul className="list-disc list-inside">
+                                    <li>
+                                        Created using ES6 classes that extend{" "}
+                                        <Code>React.Component</Code>.
+                                    </li>
+                                    <li>
+                                        Have access to lifecycle methods like{" "}
+                                        <Code>componentDidMount</Code> and{" "}
+                                        <Code>shouldComponentUpdate</Code>.
+                                    </li>
+                                    <li>
+                                        Use <Code>this.state</Code> to manage
+                                        state.
+                                    </li>
+                                </ul>
+                                <SyntaxHighlighter
+                                    language="jsx"
+                                    style={oneDark}
+                                    className="rounded mt-4 text-sm"
+                                >
+                                    {`import React,{Component} from "react";
+
+class Greeting extends Component {
+  render() {
+    return <h1>Hello, {this.props.name}!</h1>;
+  }
+}
+
+export default Greeting;`}
+                                </SyntaxHighlighter>
+                            </li>
+                        </ol>
+                        <ul className="list-disc list-inside">
+                            <span className="font-semibold">
+                                Simple Concept:
+                            </span>
+                            <ul className="list-disc list-inside">
+                                <li>
+                                    You have noticed that we have used two
+                                    methods to export the components.
+                                    <br />
+                                    i. One using{" "}
+                                    <Code>export constant . . .</Code>
+                                    <br />
+                                    ii. Anther using{" "}
+                                    <Code>export default component-name</Code>
+                                </li>
+                                <li>
+                                    If we use first method we have to use the{" "}
+                                    <Code>component-name</Code> to call this
+                                    component in another file.
+                                </li>
+                                <li>
+                                    If we use second method we can use any name
+                                    to call the component in another file.
+                                </li>
+                            </ul>
+                        </ul>
                     </p>
                 </section>
                 <p className="text-lg">
